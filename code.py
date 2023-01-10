95a7a82f9188e61a417ce891644a97751f9f1ad5
CarolynWP <99274537+CarolynWP@users.noreply.github.com>
2023-01-09 22:46:02 -0500

    finished code :)

---
+++ code.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# Created by: Carolyn Webster Pless
+# Created on: 2023/1/9
+# The start of the game
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
