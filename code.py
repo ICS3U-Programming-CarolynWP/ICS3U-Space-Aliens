0b180fc0e2262024a8fa260c7bdaa60ef144d641
Carolyn Webster Pless <carolyn.webster-pless@stu.ocsb.ca>
2023-01-17 19:04:43 +0000

    Splash screen and lasers and aliens

--- code.py
+++ code.py
@@ -10,6 +10,67 @@ import constants
 # create the game
 import stage
 import ugame
+import time
+import random
+
+# The splash scene function
+def splash_scene():
+    
+    # Getting the sound ready to play
+    splash_sound = open("splash_scene.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    
+    # Playing the sound effect
+    sound.play(splash_sound)
+    
+    # Allowing the background image to be accessed
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # Background grid
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
+    
+    # Puts the mt_game_studio image back together, since it is split up into tiles
+    # First number is x coordinate, second is y, third is image index
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+    
+    # Displaying and rendering the background which updates at 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # Background with text
+    game.layers = [background]
+    # Renders everything on the screen
+    game.render_block()
+
+    while True:
+        # Wait for 2 seconds then switch to the menu scene
+        time.sleep(2.0)
+        menu_scene()
 
 
 def menu_scene():
@@ -25,7 +86,7 @@ def menu_scene():
     )
     # Moving the text
     text1.move(5, 20)
-    text1.text("Pine Tree Studiospresents:")
+    text1.text("Happy Tree Gamespresents:")
     text.append(text1)
     text2 = stage.Text(
         width=17, height=11, font=None, palette=constants.RED_PALETTE, buffer=None
@@ -62,6 +123,14 @@ def menu_scene():
 
 # The game scene (main game)
 def game_scene():
+    
+    # Takes an alien from offscreen and moves to the screen
+    def show_raptor():
+        for raptor_number in range(len(raptors)):
+            if raptors[raptor_number].x < 0:
+                raptors[raptor_number].move(random.randint(0 + constants.SPRITE_SIZE,
+                constants.SCREEN_SIZE_X - constants.SPRITE_SIZE), constants.OFF_TOP_SCREEN)
+                break
     # Allowing the background image to be accessed
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     # Allowing the sprite image to be accessed
@@ -70,23 +139,40 @@ def game_scene():
     background = stage.Grid(
         image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
     )
+    
+    # To randomize the tiles
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            # Choose a random tile and place a random image
+            tile_picked = random.randint(0,3)
+            background.tile(x_location, y_location, tile_picked)
+   
     # Adding the lizard and placing it at point (5, 66)
     lizard = stage.Sprite(
         image_bank_sprites, 5, 75, constants.SCREEN_SIZE_Y - (2 * constants.SPRITE_SIZE)
     )
 
-    # Adding the raptor sprite
-    raptor = stage.Sprite(
-        image_bank_sprites,
-        9,
-        int(constants.SCREEN_SIZE_X / 2 - constants.SPRITE_SIZE / 2),
-        16,
-    )
-
+    # Adding the raptors to the game, stored in a list.
+    raptors = []
+    for raptor_number in range(constants.TOTAL_NUMBER_OF_ALIENS):
+        a_single_raptor = stage.Sprite(image_bank_sprites, 9,
+        constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+        raptors.append(a_single_raptor)
+    # Display 1 raptor on the screen
+    show_raptor()    
+    # Adding the lasers to the game; they are stored in a list
+    # 5 at a time
+    lasers = []
+    for laser_number in range(constants.TOTAL_NUMBER_OF_LASERS):
+        a_single_laser = stage.Sprite(image_bank_sprites, 10, 
+                                    constants.OFF_SCREEN_X, 
+                                    constants.OFF_SCREEN_Y)
+        lasers.append(a_single_laser)
+        
     # Displaying and rendering the background which updates at 60fps
     game = stage.Stage(ugame.display, constants.FPS)
     # Layers on the screen
-    game.layers = [lizard] + [raptor] + [background]
+    game.layers = raptors + lasers + [lizard] + [background]
     # Renders everything on the screen
     game.render_block()
 
@@ -151,12 +237,41 @@ def game_scene():
 
         # Once the A button is button_just_pressed, play the sound.
         if a_button == constants.button_state["button_just_pressed"]:
-            sound.play(pew_sound)
-
-        # To render and redraw the lizard sprites
-        game.render_sprites([lizard] + [raptor])
+            # Fire a laser if we have not used up all lasers
+            for laser_number in range(len(lasers)):
+                if lasers[laser_number].x < 0:
+                    lasers[laser_number].move(lizard.x, lizard.y)
+                    # Play the laser sound
+                    sound.play(pew_sound)
+                    break
+        # Checks each laser to see if it is on the screen.
+        for laser_number in range(len(lasers)):
+            if lasers[laser_number].x > 0:
+                # If the laser is on the screen, move it up the screen
+                lasers[laser_number].move(lasers[laser_number].x,
+                lasers[laser_number].y - constants.LASER_SPEED)
+                # If the laser if off the screen, move it back to the holding area
+                if lasers[laser_number].y < constants.OFF_TOP_SCREEN:
+                    lasers[laser_number].move(constants.OFF_SCREEN_X,
+                    constants.OFF_SCREEN_Y)
+        
+        # Checks each raptor to see if it is on the screen.
+        for raptor_number in range(len(raptors)):
+            if raptors[raptor_number].x > 0:
+                # If the raptor is on the screen, move it down the screen
+                raptors[raptor_number].move(raptors[raptor_number].x,
+                raptors[raptor_number].y + constants.ALIEN_SPEED)
+                # If the raptor if off the screen, move it back to the holding area
+                if raptors[raptor_number].y > constants.SCREEN_SIZE_Y:
+                    raptors[raptor_number].move(constants.OFF_SCREEN_X,
+                    constants.OFF_SCREEN_Y)
+                    show_raptor()
+        
+        # To render and redraw the sprites
+        game.render_sprites(lasers + raptors + [lizard])
+        # Wait until the refresh rate is done
         game.tick()
 
 
 if __name__ == "__main__":
-    menu_scene()
+    splash_scene()
--- constants.py
+++ constants.py
@@ -1,1 +1,34 @@
+# !/usr/bin/env python3
+# Created by: Carolyn Webster Pless
+# Created on: 2023/1/10
+# Lizard Leap Game constants
 
+SCREEN_SIZE_X = 160
+SCREEN_SIZE_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+TOTAL_NUMBER_OF_LASERS = 5
+SHIP_SPEED = 1
+ALIEN_SPEED = 1
+LASER_SPEED = 2
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_SIZE_Y + SPRITE_SIZE
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released",
+}
+
+# Palette for the text
+RED_PALETTE = (
+    b"\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+)
